304524c1715eebf6b9c42b3aaa850ef6b38f0646
AnaCarlaG <47726766+AnaCarlaG@users.noreply.github.com>
2021-04-28 21:44:21 -0300

    get dos repositories

---
+++ Chatbot/Controllers/GithubController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chatbot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GithubController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("{termoBusca}")]
+        public async Task<IList<Repository>> Repositorys(string termoBusca)
+        {
+            var github = new GitHubClient(new ProductHeaderValue("ChatBot"));
+            var request = new SearchRepositoriesRequest(termoBusca)
+            {
+                // how about we restrict the language the library is written in
+                Language = Language.CSharp,
+
+                // or go all out and search the readme, name or description
+                In = new[] { InQualifier.Readme, InQualifier.Description, InQualifier.Name },
+
+                // we can also restrict the owner of the repo if we so wish
+                User = "takenet"
+            };
+            
+            var result = await github.Search.SearchRepo(request);
+            var listRepos = result.Items;
+
+            SortedList<DateTimeOffset, Repository> sortByDate = new SortedList<DateTimeOffset, Repository>();
+            int count_repo = 0;
+
+
+            foreach (var repo in listRepos)
+            {
+                
+                sortByDate.Add(repo.CreatedAt, repo);
+                count_repo++;
+                if (count_repo == 5) break;
+            }
+
+            //foreach (var repo in sortByDate.Values)
+            //{
+            //    Console.WriteLine(repo.FullName + ": " + repo.CreatedAt);
+            //}
+            return sortByDate.Values;
+           // return listRepos.ElementAtOrDefault(0);
+        }
+    }
+}
