cbe974b57aa695c9996b3e78cb97615fb728c9c4
AnaCarlaG <acarlagomes250@gmail.com>
2021-04-29 14:07:24 -0300

    classe de request dos repository do github e classe de json document da description e fullName do repository

---
+++ Chatbot/Request/RequestGithub.cs
@@ -0,0 +1,42 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Chatbot.Request
+{
+    public class RequestGithub
+    {
+        public async Task<IList<Repository>> getRepository(string termoBusca)
+        {
+            var github = new GitHubClient(new ProductHeaderValue("ChatBot"));
+            var request = new SearchRepositoriesRequest(termoBusca)
+            {
+                // how about we restrict the language the library is written in
+                Language = Language.CSharp,
+
+                // or go all out and search the readme, name or description
+                In = new[] { InQualifier.Readme, InQualifier.Description, InQualifier.Name },
+
+                // we can also restrict the owner of the repo if we so wish
+                User = "takenet"
+            };
+
+            var result = await github.Search.SearchRepo(request);
+            var listRepos = result.Items;
+
+            SortedList<DateTimeOffset, Repository> sortByDate = new SortedList<DateTimeOffset, Repository>();
+            int count_repo = 0;
+
+
+            foreach (var repo in listRepos)
+            {
+
+                sortByDate.Add(repo.CreatedAt, repo);
+                count_repo++;
+                if (count_repo == 5) break;
+            }
+            return sortByDate.Values;
+        }
+    }
+}
---
+++ Chatbot/Send/JsonDocumentMessageSend.cs
@@ -0,0 +1,39 @@
+﻿using Lime.Protocol;
+using Octokit;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Chatbot.Send
+
+{
+    public class JsonDocumentMessageSend
+    {
+        JsonDocument jsonDocumentsDescription = new JsonDocument();
+        JsonDocument jsonDocumentsFullName = new JsonDocument();
+
+        public JsonDocument getDescription(Task<IList<Repository>> repositories)
+        {
+            int count = 0;
+            foreach (var item in repositories.Result)
+            {
+                jsonDocumentsDescription.Add(count.ToString(), item.Description);
+                count++;
+            }
+
+            jsonDocumentsDescription.SetMediaType("application/json");
+            return jsonDocumentsDescription;
+        }
+        public JsonDocument getFullName(Task<IList<Repository>> repositories)
+        {
+            int count = 0;
+            foreach (var item in repositories.Result)
+            {
+                jsonDocumentsFullName.Add(count.ToString(), item.FullName);
+                count++;
+            }
+
+            jsonDocumentsFullName.SetMediaType("application/json");
+            return jsonDocumentsFullName;
+        }
+    }
+}
