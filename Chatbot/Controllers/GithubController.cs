7f56d713bfeb1b7237431ded6ec81ffe965123c3
AnaCarlaG <acarlagomes250@gmail.com>
2021-04-29 14:05:20 -0300

    retirada do packages n usados, mudança de diretorio e mudança de name da class

--- Chatbot/Controllers/GithubController.cs
+++ Chatbot/Controllers/GithubController.cs
@@ -1,8 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Octokit;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Chatbot.Request;
+using Chatbot.Send;
+using Lime.Protocol;
+using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace Chatbot.Controllers
@@ -11,40 +10,23 @@ namespace Chatbot.Controllers
     [ApiController]
     public class GithubController : ControllerBase
     {
-
+        JsonDocumentMessageSend envio = new JsonDocumentMessageSend();
+        RequestGithub request = new RequestGithub();
         [HttpGet]
-        [Route("{termoBusca}")]
-        public async Task<IList<Repository>> Repositorys(string termoBusca)
+        [Route("description/{termoBusca}")]
+        public async Task<JsonDocument> getDescription(string termoBusca)
         {
-            var github = new GitHubClient(new ProductHeaderValue("ChatBot"));
-            var request = new SearchRepositoriesRequest(termoBusca)
-            {
-                // how about we restrict the language the library is written in
-                Language = Language.CSharp,
-
-                // or go all out and search the readme, name or description
-                In = new[] { InQualifier.Readme, InQualifier.Description, InQualifier.Name },
-
-                // we can also restrict the owner of the repo if we so wish
-                User = "takenet"
-            };
-            
-            var result = await github.Search.SearchRepo(request);
-            var listRepos = result.Items;
-
-            SortedList<DateTimeOffset, Repository> sortByDate = new SortedList<DateTimeOffset, Repository>();
-            int count_repo = 0;
-
-
-            foreach (var repo in listRepos)
-            {
-                
-                sortByDate.Add(repo.CreatedAt, repo);
-                count_repo++;
-                if (count_repo == 5) break;
-            }
-
-            return sortByDate.Values;
+            var repositories = request.getRepository(termoBusca);
+            var document = envio.getDescription(repositories);
+            return document;
+        }
+        [HttpGet]
+        [Route("fullName/{termoBusca}")]
+        public async Task<JsonDocument> getFullName(string termoBusca)
+        {
+            var repositories = request.getRepository(termoBusca);
+            var document = envio.getFullName(repositories);
+            return document;
         }
     }
 }
